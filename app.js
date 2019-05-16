93225d7e8b11a806f6df81b0715016ffb10b41a6
marvinwilliam <la805779602@hotmail.com>
2019-05-16 17:50:08 +0800

    feat: support ajv v6

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 'use strict';
 
 const assert = require('assert');
-const {
-  join,
-} = require('path');
+const { join } = require('path');
+const Ajv = require('ajv');
 
-module.exports = app => {
-  const { keyword = 'schema' } = app.config.ajv || {};
-  app.loader.loadToApp(join(app.baseDir, `app/${keyword}`), keyword, {
-    initializer(exp, { path, pathName }) {
-      assert(app.ajv.validateSchema(exp), `${path} should be a valid schema`);
-      app.ajv.addSchema(Object.assign({ $id: pathName }, exp), pathName);
-      return exp;
-    },
-  });
+class AppBootHook {
+  constructor(app) {
+    this.app = app;
+  }
 
-};
+  didLoad() {
+    const app = this.app;
+    const config = app.config.ajv;
+
+    const ajv = new Ajv(Object.assign({
+      allErrors: true, // required for custom error message
+      jsonPointers: true, // required for custom error message
+    }, config));
+    require('ajv-merge-patch')(ajv);
+    require('ajv-errors')(ajv, {
+      keepErrors: config.keepErrors || false,
+      singleError: config.singleError || false,
+    });
+    app.ajv = ajv;
+
+    const { keyword = 'schema' } = app.config.ajv || {};
+    app.loader.loadToApp(join(app.baseDir, `app/${keyword}`), keyword, {
+      match: '**/*.@(js|json)',
+      initializer(exp, { path, pathName }) {
+        assert(app.ajv.validateSchema(exp), `${path} should be a valid schema`);
+        app.ajv.addSchema(Object.assign({ $id: pathName }, exp), pathName);
+        return exp;
+      },
+    });
+  }
+}
+
+module.exports = AppBootHook;
